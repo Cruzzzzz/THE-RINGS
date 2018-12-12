0b9f03e9f55b3e3cdbd926eb42160a19c6c0453c
Cruzzzzz <45172092+Cruzzzzz@users.noreply.github.com>
2018-12-12 12:39:49 +0100

    Add files via upload

---
+++ THE RINGS.py
@@ -0,0 +1,57 @@
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('white')
+
+vera = turtle.Turtle()
+vera.color('light blue')
+vera.speed(3)
+vera.pensize(10)
+vera.penup()
+vera.goto(-250,00)
+vera.pendown()
+vera.circle(100)
+
+
+cruz = turtle.Turtle()
+cruz.color('black')
+cruz.speed(6)
+cruz.pensize(10)
+cruz.penup()
+cruz.goto(-35,00)
+cruz.pendown()
+cruz.circle(100)
+
+
+dam = turtle.Turtle()
+dam.color('red')
+dam.speed(6)
+dam.pensize(10)
+dam.penup()
+dam.goto(180,00)
+dam.pendown()
+dam.circle(100)
+
+
+kan = turtle.Turtle()
+kan.color('yellow')
+kan.speed(6)
+kan.pensize(10)
+kan.penup()
+kan.goto(-145,-110)
+kan.pendown()
+kan.circle(100)
+
+yin = turtle.Turtle()
+yin.color('light green')
+yin.speed(6)
+yin.pensize(10)
+yin.penup()
+yin.goto(75,-110)
+yin.pendown()
+yin.circle(100)
+
+
+print('the rings')
